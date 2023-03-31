ffc68a0bc15daba5ccfd1c79e0810ffd70c425e9
UnHappyAli <108374829+UnHappyAli@users.noreply.github.com>
2023-03-31 13:24:51 +0300

    Add files via upload

---
+++ servo.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+import time
+
+# Servo motorun bağlı olduğu GPIO pin numarası
+servo_pin = 18
+
+# GPIO pinlerini BCM modunda ayarla
+GPIO.setmode(GPIO.BCM)
+
+# Servo motorun bağlı olduğu pinin çıkış olarak ayarla
+GPIO.setup(servo_pin, GPIO.OUT)
+
+# PWM (Pulse-Width Modulation) nesnesi oluştur ve frekansı ayarla
+pwm = GPIO.PWM(servo_pin, 50)
+
+# PWM sinyalini başlat
+pwm.start(0)
+
+
+# Servo motoru 0 derece pozisyonda başlat
+def set_servo_angle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(servo_pin, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(servo_pin, False)
+    pwm.ChangeDutyCycle(0)
+
+# Servo motoru 180 dereceye kadar döndür
+set_servo_angle(90)
+time.sleep(4)
+set_servo_angle(135)
+time.sleep(5)
+set_servo_angle(90)
+time.sleep(6)
+set_servo_angle(45)
+time.sleep(3)
+set_servo_angle(90)
+time.sleep(5)
+set_servo_angle(45)
+time.sleep(3)
+set_servo_angle(90)
+time.sleep(6)
+set_servo_angle(60)
+time.sleep(5)
+set_servo_angle(90)
+time.sleep(1)
+
+
+
+
+# PWM sinyalini durdur ve GPIO pinlerini temizle
+pwm.stop()
+GPIO.cleanup()
